df1750616d1ad595dd4798cd8bfa13674ce87d0c
Jay Weeks <jay.patrick.weeks@gmail.com>
2017-06-19 12:55:44 -0700

    Add initial face composition

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 var clm = window.clm;
 var pModel = window.pModel;
 
+var glMatrix = require('gl-matrix')
+var vec2 = glMatrix.vec2
+
 var ctrackImage = document.getElementById('ctrack-image')
 var ctrackOverlay = document.getElementById('ctrack-overlay')
 var ctrackConvergence = document.getElementById('ctrack-convergence')
 var ctrackImageCtx = ctrackImage.getContext('2d')
 var ctrackOverlayCtx = ctrackOverlay.getContext('2d')
+
+var compositeImage = document.getElementById('composite-image')
+var compositeImageCtx = compositeImage.getContext('2d')
+
+var viewState = {
+  width: 0,
+  height: 0
+}
 
 var ctrack = new clm.tracker({
   scoreThreshold: 0.5,
@@ -40,35 +51,32 @@ function drawDebugLines (ctx, color, lines, positions) {
   ctx.stroke()
 }
 
-var faceImages = [
-  './assets/mitch/001.jpg',
-  './assets/mitch/002.jpg',
-  './assets/mitch/003.jpg',
-  './assets/mitch/004.jpg'
-]
-var faceImageIndex = 0
+var faceImagesCount = 17
+var faceImageIndex = -1
+var faceImage = null
 function loadNextFaceImage () {
-  var src = faceImages[faceImageIndex++]
-  if (!src) return
-  var image = new Image()
-  image.onload = function () {
+  if (faceImageIndex === faceImagesCount - 1) return
+  var src = './static/assets/test/' + (++faceImageIndex) + '.jpg'
+  faceImage = new Image()
+  faceImage.onload = function () {
     ctrackImageCtx.clearRect(0, 0, 600, 400)
-    ctrackImageCtx.drawImage(image, 0, 0, 600, 400)
+    ctrackImageCtx.drawImage(faceImage, 0, 0, 600, 400)
     startSearchFace()
   }
-  image.src = src
+  faceImage.src = src
 }
 
 var drawSearchProgressReq
 function drawSearchProgress () {
+  var ctx = ctrackOverlayCtx
   var convergence = ctrack.getConvergence()
   var positions = ctrack.getCurrentPosition()
 
-  ctrackOverlayCtx.clearRect(0, 0, 600, 400)
+  ctx.clearRect(0, 0, 600, 400)
   if (positions) {
     ctrack.draw(ctrackOverlay)
-    drawDebugLines(ctrackOverlayCtx, 'cyan', linesOuterFace, positions)
-    drawDebugLines(ctrackOverlayCtx, 'magenta', linesCenterFace, positions)
+    drawDebugLines(ctx, 'cyan', linesOuterFace, positions)
+    drawDebugLines(ctx, 'magenta', linesCenterFace, positions)
   }
 
   ctrackConvergence.textContent = convergence.toFixed(3)
@@ -79,6 +87,54 @@ function drawSearchProgress () {
   }
 }
 
+function getSegmentAngle (a, b) {
+  var rel = vec2.sub([], b, a)
+  return Math.atan2(rel[1], rel[0])
+}
+
+function getCentroid (out, points) {
+  vec2.set(out, 0, 0)
+  for (var i = 0; i < points.length; i++) {
+    vec2.add(out, out, points[i])
+  }
+  vec2.scale(out, out, 1 / points.length)
+  return out
+}
+
+function drawCurrentFace () {
+  var ctx = compositeImageCtx
+  var image = faceImage
+  var positions = ctrack.getCurrentPosition()
+  if (!positions) return
+
+  var width = viewState.width
+  var height = viewState.height
+
+  var posA = positions[0]
+  var posB = positions[7]
+  var posC = positions[14]
+
+  var center = getCentroid([], [posA, posB, posC])
+  var angleAC = getSegmentAngle(posA, posC)
+  var lengthAC = vec2.dist(posA, posC)
+
+  var targetLengthAC = width * 0.25
+  var scale = targetLengthAC / lengthAC
+
+  ctx.save()
+  ctx.translate(width / 2, height / 2)
+  ctx.rotate(-angleAC)
+  ctx.scale(scale, scale)
+  ctx.translate(-center[0], -center[1])
+
+  ctx.globalAlpha = 0.7
+  ctx.globalCompositeOperation = 'lighten'
+  ctx.drawImage(ctrackImage, 0, 0, 600, 400)
+  // ctx.drawImage(ctrackOverlay, 0, 0, 600, 400)
+
+  ctx.restore()
+}
+
 function startSearchFace () {
   console.time('searchFace')
   ctrack.start(ctrackImage)
@@ -86,13 +142,29 @@ function startSearchFace () {
 }
 
 function stopSearchFace (err) {
-  if (err) console.error(err)
+  if (err) console.warn(err, faceImageIndex)
+  if (!err) drawCurrentFace()
   console.timeEnd('searchFace')
   ctrack.stop()
   ctrack.reset()
   cancelAnimationFrame(drawSearchProgressReq)
   drawSearchProgressReq = null
-  setTimeout(loadNextFaceImage, 100)
+  setTimeout(loadNextFaceImage, 1)
+}
+
+function resize () {
+  var width = window.innerWidth
+  var height = window.innerHeight
+  viewState.width = width
+  viewState.height = height
+  resizeCanvas(compositeImage, width, height)
+}
+
+function resizeCanvas (canvas, width, height) {
+  canvas.width = width
+  canvas.height = height
+  canvas.style.width = width + 'px'
+  canvas.style.height = height + 'px'
 }
 
 // detect if tracker fails to find a face
@@ -110,6 +182,9 @@ document.addEventListener('clmtrackrConverged', function (event) {
   stopSearchFace()
 }, false)
 
+window.addEventListener('resize', resize)
+
 // --------------------------------------------------
 
+resize()
 loadNextFaceImage()
