e6ca4ca01f50ac188d78e3188524e11769e460c6
Jay Weeks <jay.patrick.weeks@gmail.com>
2017-06-22 10:52:19 -0700

    Update build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,6 @@
 PATH=$(npm bin):$PATH
+rm build/bundle-clmtrackr.js
+rm build/bundle-app.js
 uglifyjs ./lib/clmtrackr/clmtrackr.js ./lib/clmtrackr/models/model_pca_10_svm.js \
   --compress --mangle > build/bundle-clmtrackr.js
 browserify -t [ glslify ] src/index.js | uglifyjs --compress --mangle > build/bundle-app.js
